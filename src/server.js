e39d828976f6dfb50fc7de4c5b9ed8bcc2700c02
Aacgectyuoki <maximiliandellthib@mail.adelphi.edu>
2021-12-27 12:23:56 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/yemensuper'));
+app.use(express.static('./dist/YemenSuper'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/yemensuper/'}),
+    res.sendFile('index.html', {root: 'dist/YemenSuper/'}),
 );
 
 // Start the app by listening on the default Heroku port
