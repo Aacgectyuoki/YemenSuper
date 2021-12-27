4b86a9a1fa2dbbdb55f2b5a30efffd48dece76a0
aacgectyuoki <maxdell-thibodeau@hotmail.com>
2021-12-27 12:15:46 -0500

    third commit

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -10,6 +10,8 @@ export class CartComponent implements OnInit {
 
   public products : any = [];
   public grandTotal !: number;
+  paymentHandler: any = null;
+  //invokeStripe: any;
   constructor(private cartService : CartService) { }
 
   ngOnInit(): void {
@@ -18,12 +20,52 @@ export class CartComponent implements OnInit {
       this.products = res;
       this.grandTotal = this.cartService.getTotalPrice();
     })
+    this.invokeStripe()
   }
+
   removeItem(item: any){
     this.cartService.removeCartItem(item);
   }
+
   emptycart(){
     this.cartService.removeAllCart();
   }
 
+  makePayment(amount:any){
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key:
+      'pk_test_51KB4AqHzyheHHaqUxDultJvUHaZMnbUS1s7KBJM176xaU0Wyk0HLZeLJnBAPRWw8mpZSX7GsagJAgf1sxP4CdcE500cZ37DCeY',
+      locale: 'auto',
+      token: function(stripeToken: any){
+        console.log(stripeToken.card);
+        alert("Stripe Token Generated!");
+      },
+    });
+
+    paymentHandler.open({
+      name: 'max',
+      description: 'seller',
+      amount: amount * 100,
+    });
+  }
+
+  invokeStripe(){
+    if (!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement('script');
+      script.id = 'stripe-script';
+      script.type = 'text/javascript';
+      script.src = "https://checkout.stripe.com/checkout.js";
+      script.onload = () => {
+        this.paymentHandler = (<any>window).StripeCheckout.configure({
+        key: 'pk_test_51KB4AqHzyheHHaqUxDultJvUHaZMnbUS1s7KBJM176xaU0Wyk0HLZeLJnBAPRWw8mpZSX7GsagJAgf1sxP4CdcE500cZ37DCeY',
+        locale: 'auto',
+        token: function (stripeToken: any) {
+          console.log(stripeToken)
+          alert('Payment has been successfull!');
+        }
+      });
+    }
+    window.document.body.appendChild(script);
+    }
+  }
 }
