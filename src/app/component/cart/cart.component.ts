1b0ffb0e4046963fd2d733a54f65e7685fc95af7
aacgectyuoki <maxdell-thibodeau@hotmail.com>
2022-01-29 10:36:39 -0500

    1-29-22

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -14,9 +14,6 @@ export class CartComponent implements OnInit {
   public products : any = [];
   public grandTotal !: number;
   paymentHandler: any = null;
-  success: boolean = false;
-  failure: boolean = false;
-  //invokeStripe: any;
   constructor(private cartService : CartService, private checkout: CheckoutService) { }
 
   ngOnInit(): void {
@@ -25,7 +22,6 @@ export class CartComponent implements OnInit {
       this.products = res;
       this.grandTotal = this.cartService.getTotalPrice();
     })
-    this.invokeStripe()
   }
 
   totalItemPayment(quantity: number, price: number){
@@ -38,59 +34,5 @@ export class CartComponent implements OnInit {
 
   emptycart(){
     this.cartService.removeAllCart();
-  }
-
-  makePayment(amount:number){
-    const paymentHandler = (<any>window).StripeCheckout.configure({
-      key:
-      'pk_test_51KB4AqHzyheHHaqUxDultJvUHaZMnbUS1s7KBJM176xaU0Wyk0HLZeLJnBAPRWw8mpZSX7GsagJAgf1sxP4CdcE500cZ37DCeY',
-      locale: 'auto',
-      token: function(stripeToken: any){
-        console.log(stripeToken.card);
-        paymentstripe(stripeToken.card);
-        alert("Stripe Token Generated!");
-      },
-    });
-
-    const paymentstripe = (stripeToken: any) => {
-      this.checkout.makePayment(stripeToken).subscribe((data: any) => {
-        console.log(data);
-        if (data.data === "success") {
-          this.success = true
-        }
-        else {
-          this.failure = true
-        }
-      });
-    };
-
-    paymentHandler.open({
-      name: 'Payment',
-      description: 'Please use your credit/debit card to pay for your item(s)',
-      amount: amount * 100,
-    });
-  }
-
-  invokeStripe(){
-    if (!window.document.getElementById('stripe-script')) {
-      const script = window.document.createElement('script');
-      script.id = 'stripe-script';
-      script.type = 'text/javascript';
-      script.src = "https://checkout.stripe.com/checkout.js";
-      script.onload = () => {
-        this.paymentHandler = (<any>window).StripeCheckout.configure({
-        key: 'pk_test_51KB4AqHzyheHHaqUxDultJvUHaZMnbUS1s7KBJM176xaU0Wyk0HLZeLJnBAPRWw8mpZSX7GsagJAgf1sxP4CdcE500cZ37DCeY',
-        locale: 'auto',
-        token: function (stripeToken: any) {
-          console.log(stripeToken)
-          //alert('Payment has been successfull!');
-          // reduce the quantity of the item by 1 once the buyer's card went through
-          // the buyer should recieve an email to notify that they have paid
-          // the buyer should have the option in their email to cancel their payment, bringing the quantity back
-        }
-      });
-    }
-    window.document.body.appendChild(script);
-    }
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,110 +1,53 @@
-/*const express = require("express");
-const bodyparser = require('body-parser')
-const app = express();
-app.use(bodyparser.urlencoded({ extended: false }))
-app.use(bodyparser.json())
-const stripe = require("stripe")("sk_test_51KB4AqHzyheHHaqUvBTsbgzymrNbpsSON0CRTtBAehhr5T3EnuVZTQmEB7FVdoeMAk9WGonE8KGThs7nCiEOzZ8D00hceShXui");
-const cors = require('cors')
-
-app.use(cors())
-
-app.post('/checkout', async(req, res) => {
-    try {
-        console.log(req.body);
-        token = req.body.token
-      const customer = stripe.customers.create({
-          email: "maxdell-thibodeau@hotmail.com",
-          source: token.id
-        })
-        .then((customer) => {
-          console.log(customer);
-          return stripe.charges.create({
-            amount: 1000,
-            description: "Test Purchase using express and Node",
-            currency: "USD",
-            customer: customer.id,
-          });
-        })
-        .then((charge) => {
-          console.log(charge);
-            res.json({
-              data:"success"
-          })
-        })
-        .catch((err) => {
-            res.json({
-              data: "failure",
-            });
-        });
-      return true;
-    } catch (error) {
-      return false;
-    }
-})
-
-app.listen(4200, () => {
-    console.log("App is listening on Port 4200")
-})
-
-// @ts-ignore
-const stripe = require('stripe')('sk_test_51KB4AqHzyheHHaqUvBTsbgzymrNbpsSON0CRTtBAehhr5T3EnuVZTQmEB7FVdoeMAk9WGonE8KGThs7nCiEOzZ8D00hceShXui');
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-//const checkoutButton = document.getElementById('checkout-button');
-
-const YOUR_DOMAIN = 'http://localhost:4200/';
-
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: '{{PRICE_ID}}',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-  });
-
-  res.redirect(303, session.url);
-});
-
-app.listen(4200, () => console.log('Running on port 4200'));
-*/
 // This is your test secret API key.
 const stripe = require('stripe')('sk_test_51KB4AqHzyheHHaqUvBTsbgzymrNbpsSON0CRTtBAehhr5T3EnuVZTQmEB7FVdoeMAk9WGonE8KGThs7nCiEOzZ8D00hceShXui');
 const express = require('express');
+const bodyParser = require("body-parser");
+const { resolve } = require('path');
 const app = express();
-app.use(express.static('public'));
+app.use(express.static('app/component/cart'));
 
 const YOUR_DOMAIN = 'http://localhost:4200';
+
+app.use((req, res, next) => {
+  bodyParser.json()(req, res, next);
+});
+
+app.get("/", (req, res) => {
+  const path = resolve("app/component/cart" + "/cart.component.html");
+  res.sendFile(path);
+})
+/*
+app.get("/success", (req, res) => {
+  const path = resolve("app/component/cart" + "/success.html");
+  res.sendFile(path);
+})*/
+
+app.get("/checkout-session", async (req, res) => {
+  // @ts-ignore
+  const session = await stripe.checkout.sessions.retrieve(req.query.id, {
+    expand: ['line_items']
+  });
+  res.json(session);
+})
 
 app.post('/create-checkout-session', async (req, res) => {
   // @ts-ignore
   const session = await stripe.checkout.sessions.create({
+    success_url: `${YOUR_DOMAIN}/success?id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    mode: 'payment',
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-       /* price_data: {
-          currency: "usd",
-          product_data: {
-            name: "baby clothes",
-          }
-          unit_amount: 1,
-        }*/
-        price: 1,
-        quantity: 1,
+        price: 'price_1KLvZyHzyheHHaqUkEEe52m6',
+        quantity: req.body.quantity,
       },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    ]
   });
-
-  res.redirect(303, session.url);
+  res.json({
+    id: session.id,
+  });
+  return session;
 });
 
-app.listen(4200, () => console.log('Running on port 4200'));
+app.listen(4200, () => console.log('Running on port 4242'));
