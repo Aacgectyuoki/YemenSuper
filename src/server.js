303e215f4e9c7d3894a009ea670c7014df9959b6
Aacgectyuoki <maximiliandellthib@mail.adelphi.edu>
2022-01-30 21:08:41 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -26,4 +26,4 @@ app.post('/create-checkout-session', async (req, res) => {
   res.json({ id: session.id });
 });
 
-app.listen(4242, () => console.log(`Listening on port ${4242}!`));
+app.listen(4200, () => console.log(`Listening on port ${4200}!`));
